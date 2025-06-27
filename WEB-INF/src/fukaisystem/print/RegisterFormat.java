982f56b1f3cc76b01fa60714da3dd818983f0885
Masahiro_KAMEURA <gitlab.masahiro_kameura@proservid.co.jp>
2025-06-28 02:47:21 +0900

    フォーマットが登録できないバグを修正

--- WEB-INF/src/fukaisystem/print/RegisterFormat.java
+++ WEB-INF/src/fukaisystem/print/RegisterFormat.java
@@ -7,12 +7,12 @@ import javax.servlet.ServletResponse;
 
 import com.proservid.print.dao.RegisterFormatDAO;
 import fukaisystem.ServiceFoundation;
-import print.dto.FormatDTO;
+import fukaisystem.dto.FormatDTO;
 
 public class RegisterFormat extends ServiceFoundation {
     @Override
     public Object transaction(Connection c, ServletResponse response, Object o) throws IOException, SQLException {
         FormatDTO dto = cast(response, o, FormatDTO.class);
-        return RegisterFormatDAO.registerFormat(c, dto.isOverwrite(), dto.getName(), dto.getformat());
+        return new RegisterFormatDAO().registerFormat(c, dto.isOverwrite(), dto.getName(), dto.getformat());
     }
 }
