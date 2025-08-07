e4108b6fdfcc254638738d2824c6c99e6eee1067
Masahiro_KAMEURA <gitlab.masahiro_kameura@proservid.co.jp>
2025-08-07 12:06:13 +0900

    見積書の宛名を選択方式に変更

--- WEB-INF/src/fukaisystem/application/business/AccountNamesRegister.java
+++ WEB-INF/src/fukaisystem/application/business/AccountNamesRegister.java
@@ -8,27 +8,30 @@ import java.util.Vector;
 
 import javax.servlet.ServletResponse;
 
-import fukaisystem.dto.DestinationDTO;
+import fukaisystem.dto.AccountNamesDTO;
 import fukaisystem.foundation.ServiceFoundation;
 
 /**
- * 指定された得意先の納入先リストを更新する
+ * 指定された得意先の宛名リストを更新する
  */
-public class DestinationRegister extends ServiceFoundation {
+public class AccountNamesRegister extends ServiceFoundation {
+
+	protected static final String[] TABLE_NAMES = { "M_見積宛名", "M_出荷宛名" };
 
 	@Override
 	public Object transaction(Connection c, ServletResponse response, Object o) throws IOException, SQLException {
 
-		DestinationDTO dto = cast(response, o, DestinationDTO.class);
+		AccountNamesDTO dto = cast(response, o, AccountNamesDTO.class);
+		int type = dto.getType();
 		int accountCD = dto.getAccountCD();
-		Vector<Vector<Object>> destinations = dto.getDestinations();
+		Vector<Vector<Object>> destinations = dto.getAccountNames();
 
 		if (accountCD < 1 || destinations == null || destinations.size() < 1) {
 			return null;
 		}
 		try (
 			PreparedStatement ps = c.prepareStatement(
-				"DELETE FROM M_納入先 WHERE 得意先CD=?"
+				"DELETE FROM " + TABLE_NAMES[type] + " WHERE 得意先CD=?"
 			);
 		) {
 			ps.setInt(1, accountCD);
@@ -36,7 +39,7 @@ public class DestinationRegister extends ServiceFoundation {
 		}
 		try (
 			PreparedStatement ps = c.prepareStatement(
-				"INSERT INTO M_納入先 VALUES(?, ?, ?)"
+				"INSERT INTO " + TABLE_NAMES[type] + " VALUES(?, ?, ?)"
 			);
 		) {
 			int i = 1;
@@ -46,7 +49,7 @@ public class DestinationRegister extends ServiceFoundation {
 					continue;
 				}
 				ps.setInt(1, accountCD);
-				ps.setInt(2, i++); // 納入先CD
+				ps.setInt(2, i++); // 宛名ID
 				ps.setString(3, destination);
 				ps.addBatch();
 			}
--- WEB-INF/src/fukaisystem/application/business/GetAccountNames.java
+++ WEB-INF/src/fukaisystem/application/business/GetAccountNames.java
@@ -12,29 +12,31 @@ import javax.servlet.ServletResponse;
 import fukaisystem.foundation.ServiceFoundation;
 
 /**
- * 入力された得意先の納入先リストを取得する
+ * 入力された得意先の宛名リストを取得する
  */
-public class GetDestination extends ServiceFoundation {
+public class GetAccountNames extends ServiceFoundation {
+
+	protected static final String[] TABLE_NAMES = { "M_見積宛名", "M_出荷宛名" };
 
 	@Override
 	public Object access(Connection c, ServletResponse response, Object o) throws IOException, SQLException {
 
-		Integer code = cast(response, o, Integer.class);
-		Vector<String> destinations = new Vector<>();
-		destinations.add("");
+		int[] tableAndCode = cast(response, o, int[].class);
+		Vector<String> names = new Vector<>();
+		names.add("");
 
 		try (
 			PreparedStatement ps = c.prepareStatement(
-				"SELECT 納入先名 FROM M_納入先 WHERE 得意先CD=? ORDER BY 納入先ID"
+				"SELECT 宛名 FROM " + TABLE_NAMES[tableAndCode[0]] + " WHERE 得意先CD=? ORDER BY 宛名ID"
 			);
 		) {
-			ps.setInt(1, code);
+			ps.setInt(1, tableAndCode[1]);
 			try (ResultSet rs = ps.executeQuery();) {
 				while (rs.next()) {
-					destinations.add(rs.getString("納入先名"));
+					names.add(rs.getString("宛名"));
 				}
 			}
 		}
-		return destinations;
+		return names;
 	}
 }
